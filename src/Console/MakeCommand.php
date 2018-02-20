c36fb3938add905a42dfb4ae22cbec5106c9a356
Michał Golon <michal.golon@gmail.com>
2018-02-20 20:50:16 +0100

    Add option for list all available commands for interactive make
    Using interactive make command with '--list' option displays all
    available commands. Methods for collecting make-like commands
    and displaying list of those commands has been added. Only
    commands which name contains prefix 'make:' are considered
    make-like commands.
    
    Usage: php artisan make --list

--- src/Console/MakeCommand.php
+++ src/Console/MakeCommand.php
@@ -24,12 +24,97 @@ class MakeCommand extends Command
     protected $description = 'Run interactive make command';
 
     /**
+     * List of all make-like commands within Laravel.
+     *
+     * @var array
+     */
+    protected $illuminateCommands = [
+        'auth',
+        'command',
+        'controller',
+        'event',
+        'exception',
+        'factory',
+        'job',
+        'listener',
+        'mail',
+        'middleware',
+        'migration',
+        'model',
+        'notification',
+        'policy',
+        'provider',
+        'request',
+        'resource',
+        'rule',
+        'seeder',
+        'test',
+    ];
+
+    /**
+     * Collection of all available commands for interactive make.
+     *
+     * @var Collection|null
+     */
+    protected $availableCommands;
+
+    /**
      * Execute the console command.
      *
      * @return void
      */
     public function handle()
     {
+        $this->initAvailableCommands();
+
+        if (true === $this->option('list')) {
+            $this->displayAvailableCommands();
+
+            return;
+        }
+
         $this->info('Interactive Make!');
+    }
+
+    /**
+     * Initialize list of available commands for interactive make.
+     *
+     * @return void
+     */
+    protected function initAvailableCommands()
+    {
+        $this->availableCommands = collect(array_keys($this->getApplication()->all()))
+            ->filter(function (string $command) {
+                return Str::startsWith($command, 'make:');
+            })->map(function (string $command) {
+                return str_replace('make:', '', $command);
+            })->values();
+    }
+
+    /**
+     * Display list of all available commands for interactive make.
+     *
+     * @return void
+     */
+    protected function displayAvailableCommands()
+    {
+        $commands = $this->availableCommands
+            ->map(function (string $command) {
+                return [$command];
+            })->toArray();
+
+        $this->table(['Commands'], $commands);
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions(): array
+    {
+        return [
+            ['list', null, InputOption::VALUE_NONE, 'List all available commands.'],
+        ];
     }
 }
