15dbc837d674ba4fa0b1a2c2dc5b4115a839e2e1
Michał Golon <michal.golon@gmail.com>
2018-02-22 08:48:58 +0100

    Add arguments/options collector for model make command

---
+++ src/Console/CommandOptionsCollectors/ModelMakeCommandOptionsCollector.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace HydrefLab\Laravel\Make\Console\CommandOptionsCollectors;
+
+use Illuminate\Console\Command;
+
+class ModelMakeCommandOptionsCollector
+{
+    /**
+     * @param Command $command
+     * @return array
+     */
+    public function __invoke(Command $command): array
+    {
+        $options = [
+            'name' => $command->ask('Model name'),
+            '-p' => $command->confirm('Is the model a pivot model?'),
+            '--force' => $command->confirm('Override existing model class?'),
+        ];
+
+        if ($command->confirm('Generate a migration, factory, and resource controller for the model?')) {
+            return array_merge($options, [
+                '-a' => true,
+            ]);
+        }
+
+        $options = array_merge($options, [
+            '-f' => $command->confirm('Generate a factory for the model?'),
+            '-m' => $command->confirm('Generate a migration for the model?'),
+            '-c' => $command->confirm('Generate a controller for the model?'),
+        ]);
+
+        return array_merge($options, [
+            '-r' => $options['-c'] ? $command->confirm('Is this a resource controller?') : false,
+        ]);
+    }
+}
